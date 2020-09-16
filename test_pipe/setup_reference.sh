f46294002dbc4cb30e7a5d1fd6bb8aea1f0a6a58
Dan Stribling <ds@ufl.edu>
2020-09-16 08:53:14 -0400

    Update wget options

--- test_pipe/setup_reference.sh
+++ test_pipe/setup_reference.sh
@@ -20,13 +20,13 @@ tar -xvf test_reference.tar
 cd test_reference
 echo "Downloading grch38_1kgmaj.fa.gz"
 #curl -# --retry-max-time 150 --retry 15 -o grch38_1kgmaj.fa.gz   ftp://ftp.ccb.jhu.edu/pub/data/bowtie_indexes/grch38_1kgmaj.fa.gz
-wget --retry-connrefused  ftp://ftp.ccb.jhu.edu/pub/data/bowtie_indexes/grch38_1kgmaj.fa.gz
+wget --no-verbose --waitretry=60 --retry-connrefused  ftp://ftp.ccb.jhu.edu/pub/data/bowtie_indexes/grch38_1kgmaj.fa.gz
 gunzip -v grch38_1kgmaj.fa.gz
 
 echo "Downloading grch38_1kgmaj_bt2.zip"
 mkdir grch38_1kgmaj_bt2_db; cd grch38_1kgmaj_bt2_db
 #curl -# --retry-max-time 150 --retry 15 -o grch38_1kgmaj_bt2.zip ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/grch38_1kgmaj_bt2.zip
-wget --retry-connrefused ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/grch38_1kgmaj_bt2.zip
+wget --no-verbose --waitretry=60 --retry-connrefused ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/grch38_1kgmaj_bt2.zip
 unzip grch38_1kgmaj_bt2.zip
 echo "Done with reference downloads."
 cd ../../
